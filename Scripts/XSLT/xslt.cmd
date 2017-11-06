be162692123ea0b9bcd57374ada0ce3a7f3b8a50
cneud <clemens.neudecker@gmail.com>
2017-11-06 17:47:31 +0100

    translate xslt.cmd to English

--- Scripts/XSLT/xslt.cmd
+++ Scripts/XSLT/xslt.cmd
@@ -1,28 +1,28 @@
 @REM Set the Java classpath to include xalan.jar, serializer.jar, xml-apis.jar, and xercesImpl.jar -- or another conformant XML Parser -- (see Plugging in the Transformer and XML parser).
 @REM Call java and the Process class with the appropriate flags and arguments (described below). The following command line, for example, includes the -IN, -XSL, and -OUT flags with their accompanying arguments -- the XML source document, the XSL stylesheet, and the output file:
-@REM  java org.apache.xalan.xslt.Process -IN foo.xml -XSL foo.xsl -OUT foo.out 
+@REM  java org.apache.xalan.xslt.Process -IN foo.xml -XSL foo.xsl -OUT foo.out
 
 @setlocal
 
 
 @echo off
-@REM le fichier xml à parser
-@if %1"O" EQU "O" goto erreur
+@REM the xml file to parse
+@if %1"O" EQU "O" goto error
 
-@REM la feuilles xslt
-@if %2"O" EQU "O" goto erreur
+@REM xslt stylesheets
+@if %2"O" EQU "O" goto error
 
-@REM le nom du fichier en sortie
-@if %3"O" EQU "O" goto erreur
+@REM name of the output file
+@if %3"O" EQU "O" goto error
 
-@REM l'extension du fichier en sortie
-@if %4"O" EQU "O" goto erreur
+@REM file extension of the output file
+@if %4"O" EQU "O" goto error
 
-@REM parametre XSL : id du document
-@if %5"O" EQU "O" goto erreur
+@REM XSL parameter: document ID
+@if %5"O" EQU "O" goto error
 
-@REM le mode : simulation ou traitement
-@if %6"O" EQU "O" goto erreur
+@REM choice of simulation of processing mode
+@if %6"O" EQU "O" goto error
 
 PATH=%PATH%;"C:\Program Files (x86)\Java\jre6\bin"
 
@@ -34,13 +34,13 @@ if "%6%"=="-exec" (@echo ...
 ) else (
 echo "C:\Program Files (x86)\Java\jre6\bin\java.exe -Xmx1024m -classpath ... org.apache.xalan.xslt.Process  -IN %1  -XSL %2 -OUT %3.%4 -PARAM idDoc %5" )
 
-@goto fin
+@goto end
 
-:erreur
-@echo Usage : %0 fichier.xml feuille.xsl fichier_out format id_doc -sim/-exec
-goto fin
+:error
+@echo Usage : %0 file.xml stylesheet.xsl file_out format id_doc -sim/-exec
+goto end
 
 
-:fin
+:end
 
 echo.
